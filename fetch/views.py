fc2b1eb49adfb3512c4326d81218a1939f9a8928
akgnit <akgnit7@gmail.com>
2018-06-13 13:28:22 +0530

    add

--- fetch/views.py
+++ fetch/views.py
@@ -4,5 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
 def hello(request):
-   return render(request, "myapp/template/hello.html", {})
+   return render(request, "fetch/template/hello.html", {})
+
+def hello(request):
+   return HttpResponse("WELCOME")
